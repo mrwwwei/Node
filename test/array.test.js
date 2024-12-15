e0df781370414972cad2a461ee184470790242b1
sahikaru <sahikaru88@gmail.com>
2024-12-15 21:45:54 +0800

    test: add test for array remove last item

--- test/array.test.js
+++ test/array.test.js
@@ -7,9 +7,9 @@ describe('Test My Array', () => {
         let newArray = new myArray()
         newArray.addItem(1)
         newArray.addItem(2)
-        let result = newArray.editItem(1, 3)
+        let result = newArray.editItem(3, 3)
 
-        expect(result).toEqual([1,3])
+        expect(result).toEqual([1,2,undefined,3])
     })
 
     // 2. test add one item
@@ -18,7 +18,19 @@ describe('Test My Array', () => {
         let result = newArray.addItem(1)
         expect(result).toEqual([1])
     })
+
     // 3. test remove last item
+    test('add remove last item', () => {
+        let newArray = new myArray()
+        newArray.addItem(1)
+        newArray.addItem(2)
+        newArray.addItem(3)
+        newArray.addItem(4)
+
+        let result = newArray.removeLastItem(4)
+
+        expect(result).toEqual([1,2,3])
+    })
 
     // 4. test remove first item
 
