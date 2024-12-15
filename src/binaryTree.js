eff8793900d60a6aeb425802e6e15a79f8197442
sahikaru <sahikaru88@gmail.com>
2024-12-15 12:17:05 +0800

    change binary

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -1,74 +1,58 @@
-class TreeNode{
-    constructor(data){
-        this.data=data;
-        this.left=null;
-        this.right=null;
+class TreeNode {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class BinaryTree{
-    constructor(){
-        this.root=null;
+class BinaryTree {
+    constructor() {
+        this.root = null;
     }
-    insert(data){
-        const newNode=new TreeNode(data);
-        if(!this.root){
-            this.root=newNode;
+    insert(data, node) {
+        const newNode = new TreeNode(data);
+        if (!this.root) {
+            this.root = newNode;
             return;
         }
-        let current=this.root;
-        while(true){
-            if(data<current.data){
-                if(!current.left){
-                    current.left=newNode;
-                    break;
-                }
-                current=current.left;
-            }else{
-                if(!current.right){
-                    current.right=newNode;
-                    break;
-                }
-                current=current.right;
-            
+        let current = node || this.root
+        if (data < current.data) {
+            if (!current.left) {
+                current.left = newNode;
+                return
             }
+            return this.insert(data, current.left)
+        } else {
+            if (!current.right) {
+                current.right = newNode;
+                return
+            }
+            return this.insert(data, current.right)
         }
     }
-    inorderTraversal(node=this.root){
-        if(node){
+    inorderTraversal(node = this.root) {
+        if (node) {
             this.inorderTraversal(node.left);
-            console.log(node.data);
             this.inorderTraversal(node.right);
         }
     }
-    preorderTraversal(node=this.root){
-        if(node){
+    preorderTraversal(node = this.root) {
+        if (node) {
             console.log(node.data);
             this.preorderTraversal(node.left);
             this.preorderTraversal(node.right);
         }
     }
-    postorderTraversal(node=this.root){
-        if(node){
+    postorderTraversal(node = this.root) {
+        if (node) {
             this.postorderTraversal(node.left);
-            this.inorderTraversal(node.right);
+            this.postorderTraversal(node.right);
             console.log(node.data);
         }
     }
 }
 
-
-const binaryTree=new BinaryTree();
-binaryTree.insert(5);
-binaryTree.insert(3);
-binaryTree.insert(7);
-binaryTree.insert(2);
-binaryTree.insert(4);
-
-
-console.log('中序遍历');
-binaryTree.inorderTraversal();
-binaryTree.preorderTraversal();
-binaryTree.postorderTraversal();
-
-module.exports={binaryTree};
+module.exports = {
+    BinaryTree
+};
